17049537e0e92f2f796dfbc18c645939b0199629
jmgomez <info@jmgomez.me>
2024-03-02 18:50:26 +0000

    small fix

--- src/buildscripts/nuecompilation/nuecompilation.nim
+++ src/buildscripts/nuecompilation/nuecompilation.nim
@@ -166,7 +166,8 @@ proc setPath(pluginDir: string) =
   switch("path", pluginDir / "src/nimforue")
   switch("path", pluginDir / "src/nimforue/game")
   switch("path", pluginDir / "src/nimforue/unreal")
-  switch("path", pluginDir / "src/nimforue/unreal/bindings/imported")
+  #switch("path", pluginDir / "src/nimforue/unreal/bindings/imported")
+  switch("path", pluginDir / "src/nimforue/unreal/bindings/exported") #temp
   switch("path", pluginDir / "src/nimforue/vm")
   when defined(nimsuggest): #only needed for script.nim and nimsuggest
     switch("path", pluginDir / "src/nimforue/unreal/bindings/vm")
